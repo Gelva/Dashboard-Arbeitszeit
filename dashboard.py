8f6eee369c9056c21bcd6bc07e32e43d1e952ea6
Gelva <usgdb@hotmail.com>
2022-01-03 17:10:23 +0100

    update 3

--- dashboard.py
+++ dashboard.py
@@ -1,46 +1,46 @@
 import dash
 import dash_html_components as html
-import dash_core_components as dcc
 import plotly.graph_objects as go
-import plotly.express as px 
-from dash.dependencies import Input, Output 
+import dash_core_components as dcc
+import plotly.express as px
+from dash.dependencies import Input, Output
 
 
-# init dash app
 app = dash.Dash()
+
 df = px.data.stocks()
 
-def stock_prices():
-    # Function for creating line chart showing Google stock prices over time 
-    fig = go.Figure([go.Scatter(x = df['date'], y = df['GOOG'],\
-                     line = dict(color = 'firebrick', width = 4), name = 'Google')
-                     ])
-    fig.update_layout(title = 'Prices over time',
-                      xaxis_title = 'Dates',
-                      yaxis_title = 'Prices'
-                      )
-    return fig
 
 app.layout = html.Div(id = 'parent', children = [
     html.H1(id = 'H1', children = 'Styling using html components', style = {'textAlign':'center',\
                                             'marginTop':40,'marginBottom':40}),
 
-        
-        dcc.Graph(id = 'line_plot', figure = stock_prices())    
-    ]
-                     )
+        dcc.Dropdown( id = 'dropdown',
+        options = [
+            {'label':'Google', 'value':'GOOG' },
+            {'label': 'Apple', 'value':'AAPL'},
+            {'label': 'Amazon', 'value':'AMZN'},
+            ],
+        value = 'GOOG'),
+        dcc.Graph(id = 'bar_plot')
+    ])
+    
+    
+@app.callback(Output(component_id='bar_plot', component_property= 'figure'),
+              [Input(component_id='dropdown', component_property= 'value')])
+def graph_update(dropdown_value):
+    print(dropdown_value)
+    fig = go.Figure([go.Scatter(x = df['date'], y = df['{}'.format(dropdown_value)],\
+                     line = dict(color = 'firebrick', width = 4))
+                     ])
+    
+    fig.update_layout(title = 'Stock prices over time',
+                      xaxis_title = 'Dates',
+                      yaxis_title = 'Prices'
+                      )
+    return fig  
 
-dcc.Dropdown( id = 'dropdown',
-            options = [
-                {'label':'Google', 'value':'GOOG' },
-                {'label': 'Apple', 'value':'AAPL'},
-                {'label': 'Amazon', 'value':'AMZN'},
-                ],
-            value = 'GOOGL'       
-            )
 
-
-                     
 
 if __name__ == '__main__': 
     app.run_server()
